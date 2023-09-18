2f23259e1e200e19e84e7c67e0244117a9f5019a
kerian <18700872228@163.com>
2023-09-18 18:52:06 +0800

    update

---
+++ src/components/Btn/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <button></button>
+  </div>
+</template>
+
+<script setup lang="ts" name="<button></button>">
+
+</script>
+<style lang="less" scoped></style>
+<style lang="less"></style>
