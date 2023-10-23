bc26de8fe391c23d0b38d6023096c801a38b8c82
kerian <18700872228@163.com>
2023-10-24 01:18:42 +0800

    update: axios config

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosInstance, AxiosError, AxiosResponse, AxiosRequestConfig } from 'axios';
-
+import { ElMessage } from 'element-plus';
 const service: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API as string, //VITE_BASE_API是在.env文件中定义的
   timeout: 5000
 });
 
-service.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-// axios.defaults.headers=''
 service.interceptors.request.use(
   (config: AxiosRequestConfig) => {
     return config;
@@ -26,8 +25,38 @@ service.interceptors.response.use(
   },
   (error: AxiosError) => {
     console.log(error);
+    let message = ''
+    //http状态码：400 401 403 404 500 501 502 503 504
+    const status = error.response && error.response.status
+    switch (status) {
+      case 400:
+        message = '请求参数错误'
+        break
+      case 401:
+        message = '身份信息无效'
+        break
+      case 403:
+        message = '没有操作权限'
+        break
+      case 404:
+        message = '请求资源不存在'
+        break
+      case 500:
+        message = '服务器内部错误'
+        break
+      case 503:
+        message = '服务器正在维护'
+        break
+      case 504:
+        message = '服务器超时'
+        break
+      default:
+        message = '未知错误'
+        break
+    }
+    //利用element-ui Message做消息提示
+    ElMessage.error(message);
     return Promise.reject();
   }
 );
-
 export default service;
