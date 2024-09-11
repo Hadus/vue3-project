c9d5099ed74d162e6001b2cc146bfe99d8f9fe10
郑攀 <zhengpan@sonicom.com.cn>
2024-09-11 11:51:44 +0800

    optimize: drawer component

---
+++ src/views/user/setting/components/cm-drawer/type.ts
@@ -0,0 +1,18 @@
+export type DrawerType = 'add' | 'edit' | 'detail';
+type MoreBtnsType = {
+  text: string;
+  type?: 'primary';
+  status?: 'normal' | 'warning' | 'success' | 'danger';
+  icon?: string;
+  onClick?: (cb: (isOk?: boolean) => void) => void;
+};
+
+export type DrawerPropsType = {
+  visible: boolean;
+  title: string;
+  drawerType: DrawerType;
+  width?: number;
+  okText?: string;
+  cancelText?: string;
+  footerMoreBtns?: MoreBtnsType[];
+};
